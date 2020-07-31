b1a6e2332cce0de86385d8d6de8e54bf8400f515
darianelourdes <darianelsantana@gmail.com>
2020-07-30 22:23:00 -0300

    feat: create calculation function

--- App.js
+++ App.js
@@ -1,50 +1,71 @@
 import React from 'react';
-import { Text, TextInput, View, TouchableOpacity, StyleSheet } from 'react-native';
+import { Text, TextInput, View, TouchableOpacity } from 'react-native';
+
+import styles from './styles';
+
+export default class App extends React.Component {
+  state = {
+    peso: 0,
+    altura: 0,
+    imc: 0,
+    legenda: 'Indeterminado'
+  };
+
+  calcularIMC = () => {
+    const resultado = this.state.peso / (this.state.altura * this.state.altura);
+
+    this.setState({
+      imc: resultado
+    });
+
+    if(resultado < 18.5) {
+      this.setState({
+        legenda: 'Magreza'
+      });
+    } else if(resultado >= 18.5 && resultado < 25) {
+      this.setState({
+        legenda: 'Normal'
+      });
+    } else if(resultado >= 25 && resultado < 30) {
+      this.setState({
+        legenda: 'Sobrepeso'
+      });
+    } else if(resultado >= 30 && resultado < 40) {
+      this.setState({
+        legenda: 'Obesidade'
+      });
+    } else if(resultado >= 40) {
+      this.setState({
+        legenda: 'Obesidade Grave'
+      });
+    }
+  }
 
 
-export default function App() {
+  render() {
+    return (
+      <View style={styles.app}>
+        <Text style={styles.legenda}>Seu IMC</Text>
 
-  return (
-    <View style={styles.app}>
-      <Text style={styles.legenda}>Seu IMC</Text>
+        <View>
+          <Text style={styles.resultado}>{this.state.imc}</Text>
+          <Text style={styles.diagnostico}>{this.state.legenda}</Text>
+        </View>
 
-      <View>
-        <Text style={styles.resultado}>25</Text>
-        <Text style={styles.diagnostico}>Normal</Text>
+        <View>
+          <TextInput style={styles.peso} />
+          <TextInput style={styles.altura} />
+
+          <TouchableOpacity onPress={this.calcularIMC} style={styles.button}>
+            <Text style={styles.buttonText}>Calcular</Text>
+          </TouchableOpacity>
+
+        </View>
       </View>
+    );
+  }
 
-      <View>
-        <TextInput style={styles.peso} />
-        <TextInput style={styles.altura} />
 
-      </View>
-    </View>
-  );
 }
 
-const styles = StyleSheet.create({
-  app: {
-    padding: 30,
-  },
-  legenda: {
-    textAlign: 'center',
-    fontWeight: 'bold',
-  },
-  resultado: {
-    textAlign: 'center',
-    fontSize: 22,
-    fontWeight: 'bold',
-  },
-  diagnostico: {
-    textAlign: 'center',
-    fontSize: 16,
-  },
-  peso: {
-    borderColor: '#000',
-    borderWidth: 1,
-  },
-  altura: {
-    borderColor: '#000',
-    borderWidth: 1,
-  }
-});
+
---
+++ styles.js
@@ -0,0 +1,41 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  app: {
+    padding: 30,
+  },
+  legenda: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  resultado: {
+    textAlign: 'center',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  diagnostico: {
+    textAlign: 'center',
+    fontSize: 16,
+  },
+  peso: {
+    borderColor: '#000',
+    borderWidth: 1,
+  },
+  altura: {
+    borderColor: '#000',
+    borderWidth: 1,
+  },
+  button: {
+    marginTop: 20,
+    height: 50,
+    backgroundColor: '#00cec9',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  }
+});
