b31206f45d16d31d2ba6140ff69e4e21e20878ad
darianelourdes <darianelsantana@gmail.com>
2020-08-04 22:37:42 -0300

    refactor: add react hooks

--- src/index.js
+++ src/index.js
@@ -1,64 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, TextInput, View, TouchableOpacity, Keyboard, TouchableWithoutFeedback, KeyboardAvoidingView } from 'react-native';
 
 import styles from './styles';
 
-export default class Main extends React.Component {
-  state = {
-    peso: 0,
-    altura: 0,
-    imc: 0,
-    legenda: 'Indeterminado',
-    cor: '#ffeaa7',
-  };
+export default function Main() {
+  const [peso, setPeso] = useState(0);
+  const [altura, setAltura] = useState(0);
+  const [imc, setImc] = useState(0);
+  const [legenda, setLegenda] = useState('indeterminado');
+  const [cor, setCor] = useState('#ffeaa7');
 
-  calcularIMC = () => {
-    const resultado = this.state.peso / (this.state.altura * this.state.altura);
+  function calcularIMC() {
+    const resultado = peso / (altura * altura);
 
 
-    this.setState({
-      imc: Math.ceil(resultado)
-    });
+    setImc(Math.ceil(resultado));
 
 
-    if (resultado < 18.5) {
-      this.setState({
-        legenda: 'Magreza',
-        cor: '#d63031'
-      });
+    if (resultado < 18.5) {      
+        setLegenda('Magreza'),
+        setCor('#d63031');
+      
     } else if (resultado >= 18.5 && resultado < 25) {
-      this.setState({
-        legenda: 'Normal',
-        cor: '#2ecc71'
-      });
+        setLegenda('Normal'),
+        setCor('#2ecc71');
+
     } else if (resultado >= 25 && resultado < 30) {
-      this.setState({
-        legenda: 'Sobrepeso',
-        cor: '#f1c40f'
-      });
+        setLegenda('Sobrepeso'),
+        setCor('#f1c40f')
+      ;
     } else if (resultado >= 30 && resultado < 40) {
-      this.setState({
-        legenda: 'Obesidade',
-        cor: '#e67e22'
-      });
+        setLegenda('Obesidade'),
+        setCor('#e67e22');
     } else if (resultado >= 40) {
-      this.setState({
-        legenda: 'Obesidade Grave',
-        cor: '#ff6348'
-      });
+        setLegenda('Obesidade Grave'),
+        setCor('#ff6348');
     }
   }
 
-
-  render() {
     return (
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <KeyboardAvoidingView enabled={Platform.OS === 'ios'} behavior="padding" style={styles.container}>
           <Text style={styles.legenda}>Seu IMC</Text>
 
-          <View style={[styles.painel, { backgroundColor: this.state.cor }]}>
-            <Text style={styles.resultado}>{this.state.imc}</Text>
-            <Text style={styles.diagnostico}>{this.state.legenda}</Text>
+          <View style={[styles.painel, { backgroundColor: cor }]}>
+            <Text style={styles.resultado}>{imc}</Text>
+            <Text style={styles.diagnostico}>{legenda}</Text>
           </View>
 
           <View style={styles.form}>
@@ -68,7 +55,7 @@ export default class Main extends React.Component {
               placeholder="Coloque seu peso"
               keyboardType="numeric"
               onChangeText={value => {
-                this.setState({ peso: value.replace(',', '.') });
+                setPeso(value.replace(',', '.') );
               }}
             />
             <TextInput
@@ -76,11 +63,11 @@ export default class Main extends React.Component {
               placeholder="Coloque sua altura"
               keyboardType="numeric"
               onChangeText={value => {
-                this.setState({ altura: value.replace(',', '.') });
+                setAltura(value.replace(',', '.') );
               }}
             />
 
-            <TouchableOpacity onPress={this.calcularIMC} style={styles.button}>
+            <TouchableOpacity onPress={calcularIMC} style={styles.button}>
               <Text style={styles.buttonText}>Calcular</Text>
             </TouchableOpacity>
 
@@ -90,6 +77,6 @@ export default class Main extends React.Component {
     );
 
   }
-}
+
 
 
